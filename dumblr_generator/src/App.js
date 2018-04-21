7a9c104275c4a9c8ec0996eff38d5b412e56bb5c
Nick Beattie <nick@nickbytes.com>
2018-04-21 16:18:15 -0400

    chore: moving things over from old repo

--- dumblr_generator/src/App.js
+++ dumblr_generator/src/App.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
+  async writeFile() {
+    var archive = await global.DatArchive.create({
+      title: "My Site",
+      description: "Where I put my files"
+    });
+    console.log(archive.url);
+
+    await archive.writeFile(
+      "/dumblr-test.json",
+      `{
+      "posts": [
+        {
+          "id": 1,
+          "content": "this is a post"
+        }
+      ]
+    }
+    `
+    );
+  }
+
+  async updateData() {
+    const archive = new global.DatArchive(
+      "5f105947fe3ea2840a9f1416285606ec708fc75b56a232c7d74e5df629b9abe3"
+    );
+    const buf = JSON.parse(
+      await archive.readFile("./dumblr-test.json", "utf8")
+    );
+
+    // testing ability to read file
+
+    console.log(buf);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={() => this.writeFile()}>
+          Plz write to filesystem
+        </button>
+
+        <button onClick={() => this.updateData()}>buf</button>
       </div>
     );
   }
