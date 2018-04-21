199252462e45d669510883fd152fa7c0f1c64d8b
Nick Beattie <nick@nickbytes.com>
2018-04-21 16:24:59 -0400

    chore: setup

--- dumblr_generator/src/App.js
+++ dumblr_generator/src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 
 class App extends Component {
   async writeFile() {
+    // Create new DatArchive Site
     var archive = await global.DatArchive.create({
+      // add desired metadata
       title: "My Site",
       description: "Where I put my files"
     });
     console.log(archive.url);
 
+    // copy all files to DatArchive
+    // from /template_build folder + template for content.json, etc
+    // (this example only write one file)
     await archive.writeFile(
       "/dumblr-test.json",
       `{
@@ -22,26 +27,32 @@ class App extends Component {
     );
   }
 
-  async updateData() {
-    const archive = new global.DatArchive(
-      "5f105947fe3ea2840a9f1416285606ec708fc75b56a232c7d74e5df629b9abe3"
-    );
-    const buf = JSON.parse(
-      await archive.readFile("./dumblr-test.json", "utf8")
-    );
-
-    // testing ability to read file
-
-    console.log(buf);
-  }
+  // testing ability to read file
+  // async updateData() {
+  //   const archive = new global.DatArchive(
+  //     "5f105947fe3ea2840a9f1416285606ec708fc75b56a232c7d74e5df629b9abe3"
+  //   );
+  //   const buf = JSON.parse(
+  //     await archive.readFile("./dumblr-test.json", "utf8")
+  //   );
+  //   console.log(buf);
+  // }
   render() {
     return (
       <div>
-        <button onClick={() => this.writeFile()}>
-          Plz write to filesystem
-        </button>
+        <div>
+          <h1>Create your own Dumblr</h1>
+          <button onClick={() => this.writeFile()}>
+            Plz write to filesystem
+          </button>
+        </div>
 
-        <button onClick={() => this.updateData()}>buf</button>
+        <div>
+          <h2>
+            This should probably be an area that explains dumblr, or shows users
+            dumblr content
+          </h2>
+        </div>
       </div>
     );
   }
