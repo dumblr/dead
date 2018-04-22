935f8fea0b3d23d10c68f33bedf7142b268626d4
Nick Beattie <nick@nickbytes.com>
2018-04-22 14:01:46 -0400

    feat: clicking this button forks the site, gives you a url
    feat: fork create a new dumblr
    
    feat: show url

--- dumblr_generator/src/App.js
+++ dumblr_generator/src/App.js
@@ -1,64 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
+import LandingPage from "./components/LandingPage";
 
-class App extends Component {
-  async writeFile() {
-    // Create new DatArchive Site
-    var archive = await global.DatArchive.create({
-      // add desired metadata
-      title: "My Site",
-      description: "Where I put my files"
-    });
-    console.log(archive.url);
-
-    // copy all files to DatArchive
-    // from /template_build folder + template for content.json, etc
-    // (this example only write one file)
-
-    // more accurately, i think you need to read the template_directory
-    // then for each file in the directory, write to new archive.
-    await archive.writeFile(
-      "/dumblr-test.json",
-      `{
-      "posts": [
-        {
-          "id": 1,
-          "content": "this is a post"
-        }
-      ]
-    }
-    `
-    );
-  }
-
-  // testing ability to read file
-  // async updateData() {
-  //   const archive = new global.DatArchive(
-  //     "5f105947fe3ea2840a9f1416285606ec708fc75b56a232c7d74e5df629b9abe3"
-  //   );
-  //   const buf = JSON.parse(
-  //     await archive.readFile("./dumblr-test.json", "utf8")
-  //   );
-  //   console.log(buf);
-  // }
-  render() {
-    return (
-      <div>
-        <div>
-          <h1>Create your own Dumblr</h1>
-          <button onClick={() => this.writeFile()}>
-            Plz write to filesystem
-          </button>
-        </div>
-
-        <div>
-          <h2>
-            This should probably be an area that explains dumblr, or shows users
-            dumblr content
-          </h2>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => <LandingPage />;
 
 export default App;
---
+++ dumblr_generator/src/components/LandingPage/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 600px;
+  padding: 80px 10px;
+`;
+
+export const Headline = styled.h1`
+  font-size: 17px;
+  font-weight: normal;
+  line-height: 21px;
+`;
+
+export const SubHead = styled.h2`
+  font-size: 17px;
+  font-weight: normal;
+  line-height: 21px;
+`;
+
+export const CreateBtn = styled.button`
+  background-color: none;
+  border: none;
+  cursor: pointer;
+  font-family: inherit;
+  font-size: 17px;
+  font-weight: normal;
+  line-height: 21px;
+  margin: 40px -10px;
+  padding: 10px;
+`;
