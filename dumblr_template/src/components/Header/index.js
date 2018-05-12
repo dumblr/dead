7d129c1fbf03f3fe908f9c9ff512137e3f21c69c
James Ayres <jfrancis.ayres@gmail.com>
2018-05-12 13:31:50 -0400

    Begin toggle between theirs and mine

--- dumblr_template/src/components/Header/index.js
+++ dumblr_template/src/components/Header/index.js
@@ -8,9 +8,11 @@ class Header extends React.Component {
 		super(props);
 		this.state = {
 			contentSelectionOpen: false,
+			contentViewType: 'theirs',
 		}
 
-		this.toggleContentSelection = this.toggleContentSelection.bind(this)
+		this.toggleContentSelection = this.toggleContentSelection.bind(this);
+		this.toggleContentViewType = this.toggleContentViewType.bind(this);
 	}
 
 	componentDidMount() {
@@ -18,9 +20,14 @@ class Header extends React.Component {
 	}
 
 	toggleContentSelection() {
-		
 		this.setState({
 			contentSelectionOpen: !this.state.contentSelectionOpen,
+		})
+	}
+
+	toggleContentViewType(val) {
+		this.setState({
+			contentViewType: val,
 		})
 	}
 
@@ -28,13 +35,37 @@ class Header extends React.Component {
 
 		return(
 			<header className={styles.Header}>
-				<a className={styles.ContentToggle} onClick={() => this.toggleContentSelection()}>
-					{this.state.contentSelectionOpen ? 'Close' : 'Add an entry'}
-				</a>
+				<div className={styles.Header__Nav}>
+					<a className={`${styles.ContentToggle} ${this.state.contentSelectionOpen ? styles.ContentToggle_Open : ''}`} onClick={() => this.toggleContentSelection()}>
+						<img src="/images/icon-plus.svg" alt="plus icon" />
+					</a>
+					<div className={styles.ContentDisplayToggle}>
+						<a 
+							className={`
+								${styles.ContentDisplayToggle__Item} 
+								${this.state.contentViewType === 'mine' ? styles.ContentDisplayToggle__Item_Selected : ''}
+							`} 
+							onClick={() => this.toggleContentViewType('mine')}
+						>
+							<img src="/images/icon-smile.svg" alt="show my posts" />
+						</a>
+						<a 
+							className={`
+								${styles.ContentDisplayToggle__Item} 
+								${this.state.contentViewType === 'theirs' ? styles.ContentDisplayToggle__Item_Selected : ''}
+							`} 
+							onClick={() => this.toggleContentViewType('theirs')}
+						>
+							<img src="/images/icon-group.svg" alt="show their posts" />
+						</a>
+					</div>
+				</div>
+				
 
 				<ContentSelection
 					open={this.state.contentSelectionOpen}
 				/>
+
 			</header>
 		);
 	}
